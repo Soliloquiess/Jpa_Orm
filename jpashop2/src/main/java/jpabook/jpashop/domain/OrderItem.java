f87430a7639f0da3acec9760f5654cc9a1484f2b
Soliloquiess <cyh1219@naver.com>
2020-09-21 01:39:51 +0900

    Update OrderItem.java

--- jpashop2/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop2/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,27 +1,21 @@
 package jpabook.jpashop.domain;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class OrderItem {
     @Id @GeneratedValue
     @Column(name ="ORDER_ITEM_ID")
     private Long id;
-//
-//    @Column(name ="ORDER_ID")
-//    private Long orderId;
 
-    @ManyToOne
-    @JoinColumn(name= "ORDER_ID")
-    private Order order;
-//
-//    @Column(name ="ITEM_ID")
-//    private Long itemId;
+    @Column(name ="ORDER_ID")
+    private Long orderId;
 
-    @ManyToOne
-    @JoinColumn(name= "ORDER_ID")
-    private Item item;
-//
+    @Column(name ="ITEM_ID")
+    private Long itemId;
 
     private int orderPrice;
     private int count;
@@ -34,20 +28,20 @@ public class OrderItem {
         this.id = id;
     }
 
-    public Order getOrder() {
-        return order;
+    public Long getOrderId() {
+        return orderId;
     }
 
-    public void setOrder(Order order) {
-        this.order = order;
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
     }
 
-    public Item getItem() {
-        return item;
+    public Long getItemId() {
+        return itemId;
     }
 
-    public void setItem(Item item) {
-        this.item = item;
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
     }
 
     public int getOrderPrice() {
